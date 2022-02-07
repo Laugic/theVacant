7e7566245391df61c73251f1e654a96ba99a5e17
Alison Turner <alisonmoons@gmail.com>
2022-02-06 23:36:00 -0400

    Tweaked and commented ReapVFX

--- src/main/java/theVacant/vfx/ReapVFX.java
+++ src/main/java/theVacant/vfx/ReapVFX.java
@@ -13,9 +13,9 @@ import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import theVacant.util.TextureLoader;
 
 public class ReapVFX extends AbstractGameEffect {
-    private static final Texture TEXTURE = TextureLoader.getTexture("theVacantResources/images/vfx/DeathSickle.png");
-    private static final Texture shade = TextureLoader.getTexture("theVacantResources/images/vfx/blackBack.jpg");
-    private static final float DURATION = 1.0f;
+    private static final Texture TEXTURE = TextureLoader.getTexture("theVacantResources/images/vfx/DeathSickle.png"); //Change me to change the image used
+    private static final Texture BACKGROUND = TextureLoader.getTexture("theVacantResources/images/vfx/blackBack.jpg"); //Change me to change the background fade
+    private static final float DURATION = 0.75f; //Duration of the action, can lengthen or shorten as you wish. All interpolations are relative to this, so changing this is safe
     private static final float HALF_DUR = DURATION/2F;
     private final float sx, sy, tx, ty;
     private final float sa, ta, ea;
@@ -25,54 +25,54 @@ public class ReapVFX extends AbstractGameEffect {
 
     public ReapVFX() {
         super();
-        this.renderBehind = false;
-        img = new TextureAtlas.AtlasRegion(TEXTURE, 0, 0, TEXTURE.getWidth(), TEXTURE.getHeight());
-        sx = Settings.WIDTH/3F - img.getRegionWidth()/2F;
-        sy = Settings.HEIGHT/2F - img.getRegionHeight()/2F;
-        tx = Settings.WIDTH*3/5F - img.getRegionWidth()/2F;
-        ty = Settings.HEIGHT*3/5F - img.getRegionHeight()/2F;
-        sa = -20;
-        ta = 40;
-        ea = -150;
+        this.renderBehind = false; //Render over the card
+        img = new TextureAtlas.AtlasRegion(TEXTURE, 0, 0, TEXTURE.getWidth(), TEXTURE.getHeight()); //Load the image
+        sx = Settings.WIDTH/3F - img.getRegionWidth()/2F; // x coord scythe spawns at and swings back to
+        sy = Settings.HEIGHT/2F - img.getRegionHeight()/2F; // y coord scythe spawns at and swings back to
+        tx = Settings.WIDTH*3/5F - img.getRegionWidth()/2F; // x coord scythe pauses at
+        ty = Settings.HEIGHT*3/5F - img.getRegionHeight()/2F; // y coord scythe pauses at
+        sa = -20; //Angle scythe spawns at
+        ta = 60; //Angle scythe pauses at
+        ea = -150; //Angle scythe swings to
         duration = startingDuration = DURATION;
-        color = new Color(1.0F, 1.0F, 1.0F, 0.0F);
+        color = new Color(1.0F, 1.0F, 1.0F, 0.0F); //Start invisible and not null
     }
 
     @Override
     public void update() {
+        //Play sfx at the start
         if (duration == startingDuration) {
             CardCrawlGame.sound.playA("HEART_SIMPLE", MathUtils.random(0.7F, 0.8F));
         }
+        //Increment interpolation timer
         t += Gdx.graphics.getDeltaTime();
+        //Second half of the animation swings back
         if (t >= HALF_DUR) {
             this.color.a = Interpolation.pow5In.apply(1.0F, 0.0F, (t - HALF_DUR)/HALF_DUR);
             x = Interpolation.pow5In.apply(tx, sx, (t - HALF_DUR)/HALF_DUR);
             y = Interpolation.pow5In.apply(ty, sy, (t - HALF_DUR)/HALF_DUR);
             rotation = Interpolation.pow5In.apply(ta, ea, (t - HALF_DUR)/HALF_DUR);
-        } else {
+        } else { //First half of the animation grows larger and pauses
             this.color.a = Interpolation.pow5Out.apply(0.0F, 1.0F, t/HALF_DUR);
             x = Interpolation.pow5Out.apply(sx, tx, t/HALF_DUR);
             y = Interpolation.pow5Out.apply(sy, ty, t/HALF_DUR);
             rotation = Interpolation.pow5Out.apply(sa, ta, t/HALF_DUR);
-            scale = Interpolation.smooth.apply(Settings.scale*5F, Settings.scale*15F, t/HALF_DUR);
+            scale = Interpolation.smooth.apply(Settings.scale*5F, Settings.scale*10F, t/HALF_DUR);
         }
-        //rotation = Interpolation.swing.apply(10, -150, t/DURATION);
-        //x = Interpolation.swingIn.apply(tx, Settings.WIDTH/2F-img.getRegionWidth()/2F, t/DURATION);
-        //y = Interpolation.swingIn.apply(ty, Settings.HEIGHT/2F, t/DURATION);
-        //scale = Interpolation.pow5In.apply(Settings.scale*5F, Settings.scale*20F, t/DURATION);
-
+        //Reduce duration and end action if duration is 0
         duration -= Gdx.graphics.getDeltaTime();
-        if (duration < 0.0F) {
+        if (duration <= 0.0F) {
             isDone = true;
         }
     }
 
     @Override
     public void render(SpriteBatch sb) {
+        //Set the color so your alpha is applied
         sb.setColor(color);
-        //sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
-        sb.draw(shade, 0.0F, 0.0F, (float)Settings.WIDTH, (float)Settings.HEIGHT);
-        //sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        //Draw the background
+        sb.draw(BACKGROUND, 0.0F, 0.0F, (float)Settings.WIDTH, (float)Settings.HEIGHT);
+        //Draw the image
         sb.draw(this.img, this.x - this.img.packedWidth / 2.0F, this.y - this.img.packedHeight / 2.0F, this.img.packedWidth / 2.0F, this.img.packedWidth / 2.0F, this.img.packedWidth, this.img.packedHeight, this.scale, this.scale, this.rotation);
     }
 
